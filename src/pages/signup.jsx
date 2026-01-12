61843434b95fa778ba5e3c5a4a59b372cd375ef9
Puneet Kushwaha <148247654+puneetkushwaha@users.noreply.github.com>
2026-01-12 17:19:13 +0530

    message

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -22,7 +22,7 @@ const Register = () => {
         setIsLoading(true);
         // Simulate API call
         try {
-            const res = await fetch(`${BASE_URL}/api/auth/signup`, {
+            const res = await fetch(`${BASE_URL}/api/auth/register`, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(formData),
